362f2e5973dd62e40534219dd687aa0a374cf087
Dohyeon_Parrk <parkyj3213@naver.com>
2024-11-15 18:07:35 +0900

    fix : lv2 7. N+1

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -8,7 +8,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
-    List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
+    /*
+        Comment 엔티티 fetchType = LAZY 로 인해 발생하는 문제 -> JPQL Fetch Join 사용
+        - 연관된 엔티티를 사용할 때마다 해당 엔티티를 개별적으로 조회하기 때문에 N+1 번 쿼리 실행됨.
+	*/
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
+    List<Comment> findAllByTodoIdWithUser(@Param("todoId") Long todoId);
 }
