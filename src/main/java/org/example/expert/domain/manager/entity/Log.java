b90c0f0fe211efbbf68f58db0b56fa5468dc7d9a
Dohyeon_Parrk <parkyj3213@naver.com>
2024-11-19 19:07:06 +0900

    feat : lv3 11. Transaction 심화 - LogEntity 생성

---
+++ src/main/java/org/example/expert/domain/manager/entity/Log.java
@@ -0,0 +1,44 @@
+package org.example.expert.domain.manager.entity;
+
+import org.example.expert.domain.common.entity.Timestamped;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "logs")
+public class Log extends Timestamped {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private LogAction action;
+
+	@Column(nullable = false)
+	private String message;
+
+	public Log(Long id, LogAction action, String message) {
+		this.id = id;
+		this.action = action;
+		this.message = message;
+	}
+
+	public void updateAction(LogAction action, String message){
+		this.action = action;
+		this.message = message;
+	}
+}
---
+++ src/main/java/org/example/expert/domain/manager/entity/LogAction.java
@@ -0,0 +1,11 @@
+package org.example.expert.domain.manager.entity;
+
+public enum LogAction {
+
+	SAVE_SUCCESS,
+	SAVE_FAIL,
+	DELETE_SUCCESS,
+	DELETE_FAIL,
+	UPDATE_SUCCESS,
+	UPDATE_FAIL;
+}
